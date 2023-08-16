1b1b790498eb1932d433ef5aacec2ffd47d73350
Zwogli <Mathiaskohler@web.de>
2023-08-16 10:25:02 +0200

    show shuffled card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard: string = '';
+  game: Game; // variable : Typ Game
 
-  takeCard(){
-    this.pickCardAnimation = true;
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop(); // pop() =  read and delete the last array position
+      this.pickCardAnimation = true;
+
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 1800);
+    }
   }
 }
---
+++ src/models/game.ts
@@ -0,0 +1,31 @@
+export class Game {
+  public players: string[] = []; // variable : validation, string[] = string typ of array
+  public stack: string[] = [];
+  public playedCard: string[] = [];
+  public currentPlayer: number = 0;
+
+  constructor() {
+    for (let i = 1; i < 14; i++) {
+      this.stack.push('spade_' + i);
+      this.stack.push('hearts_' + i);
+      this.stack.push('diamonds_' + i);
+      this.stack.push('clubs_' + i);
+    }
+    this.shuffle(this.stack);
+  }
+
+  /**
+   * Shuffles array in place.
+   * @param {Array} a - items An array containing the items.
+   */
+  shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
+    return a;
+  }
+}
