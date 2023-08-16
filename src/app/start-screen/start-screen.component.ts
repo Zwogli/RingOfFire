b7f4d4a096f4d1ebb584baff452522fb368e73af
Zwogli <Mathiaskohler@web.de>
2023-08-16 09:02:23 +0200

    add link to newGame

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; //! need constructor!
 
 @Component({
   selector: 'app-start-screen',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class StartScreenComponent {
 
+  constructor(private router: Router){}
+
+  newGame(){
+    //Start Game
+    this.router.navigateByUrl('/game');
+  }
 }
